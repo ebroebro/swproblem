323dbc3850a7495bf6d7b1da7dba44dfd2c4afcf
ebroebro <jzzz0304@gmail.com>
2020-02-02 16:10:59 +0900

    2020 0202

--- swexpert/swexpert_1976.py
+++ swexpert/swexpert_1976.py
@@ -1,19 +1,13 @@
-#1976. 시각 덧셈
-# T=int(input())
-# for z in range(T):
-
-
-h1,m1,h2,m2 = list(map(int,input().split()))
-h,m = 0,0
-h=h1+h2
-m=m1+m2
-if h >12:
-    h-=12
-if m >=60:
-    m-=60
-    h+=1
-
-
-
-
-    # print("#{} {} {}".format(z+1,h,m))
+#1976 시각덧셈
+T=int(input())
+for z in range(T):
+    t1,m1,t2,m2 = list(map(int, input().split()))
+    t,m=0,0
+    t=t1+t2
+    m=m1+m2
+    if m>=60:
+        m-=60
+        t+=1
+    if t>12:
+        t-=12
+    print("#{} {} {}".format(z+1,t,m))
