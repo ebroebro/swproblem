0a433cf61ccbb2320587f1266e1e5ca110c74dd1
multicampus <jzzz0304@gmail.com>
2020-02-06 20:08:34 +0900

    2020 02 06

--- 백준/baekjoon_13304.py
+++ 백준/baekjoon_13304.py
@@ -13,5 +13,31 @@
 각 방별로 k 개 넘으면 따로 계산 해서 될때마다 +1
 '''
 
-
+n,k = list(map(int,input().split()))
 room = [0 for _ in range(5)]
+
+for i in range(n):
+    student = list(map(int,input().split()))
+    if student[1]==1 or student[1]==2 :  #1,2학년이면 0번방
+        room[0]+=1
+    if student[0]==0:
+        if student[1]==3 or student[1]==4:  # 여 34 1번방
+            room[1]+=1
+        if student[1] == 3 or student[1] == 4:  # 여 56 3번방
+            room[2] += 1
+    if student[1]==0:
+        if student[1]==3 or student[1]==4 : # 남 34 2번방
+            room[3]+=1
+        if student[1]==5 or student[1]==6 : # 남 56 4번방
+            room[4]+=1
+print(room)
+cnt=0
+for i in range(5):
+    if room[i]:
+        cnt+=1
+        while room[i]>k:
+            cnt+=1
+            room[i]-=k
+print(room)
+
+print(cnt)
