4bee4883dbabd1d06319fca4ceedbf9fec391cb4
ebroebro <jzzz0304@gmail.com>
2020-02-17 23:00:01 +0900

    2020 02 17

--- swexpert/swexpert 3282 knapsack.py
+++ swexpert/swexpert 3282 knapsack.py
@@ -1,13 +1,16 @@
-#4466 최대성적표 만들기
-#처음엔 그냥 그 갯수만큼 받고, 나머지 하나는 가지거 있는 것중 최저점이랑 비교해서
-#맞는지 확인하면 될거 같은데
-T=int(input())
-for z in range(T):
-    n,k = list(map(int,input().split()))
-    data_list = list(map(int,input().split()))
-    rslt_list=data_list[:k]
-    for i in range(k,n):
-        tmp = min(rslt_list)
-        if tmp < data_list[i]:
-            rslt_list[rslt_list.index(tmp)]=data_list[i]
-    print("#{} {}".format(z+1,sum(rslt_list)))
+#knapsack 3282
+#이거는 배운거 써야할듯? 그 부분집합 문제로 해결하기. 가지치기 편하려면 backtracking 쓰는게 좋을듯 ?
+# T=int(input())
+# for z in range(T):
+n,k = list(map(int,input().split()))
+v_list=[]
+c_list=[]
+for _ in range(n):
+    v,c = list(map(int,input().split()))
+    v_list.append(v)
+    c_list.append(c)
+#부피제한이 k 있는거임 가치가 높아야해
+rslt_list=[]
+
+def knapsack(data, k, input, s1,s2):
+    if k == input:
