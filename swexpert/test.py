2a083466c3ef7383d1f5ea48572fabdf6eb0842c
ebroebro <jzzz0304@gmail.com>
2020-04-13 20:17:00 +0900

    0413

--- swexpert/test.py
+++ swexpert/test.py
@@ -1,5 +1,15 @@
-from collections import deque
-
-dq=deque('123')
-dq.extendleft('123')
-print(dq[::-1])
+lines=['헛소리',
+'중앞',
+'정민기 30',
+'벅수호 50',
+'헛소리',
+'헛소리']
+i=0
+for line in lines:
+    if line == '헛소리':
+        print('헛소리')
+        while i < len(lines) :
+            print(lines[i])
+            if lines[i] == "" :
+                break
+            i = i + 1
